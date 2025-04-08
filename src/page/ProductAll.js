461b9ea883fe566071c6854dd75ae4b578f7c7ec
jihyun1029 <parkji2028@naver.com>
2025-04-08 11:06:42 +0900

    API 부르기

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ProductCard from "../component/ProductCard";
 
 const ProductAll = () => {
+    const [productList, setProductList] = useState([]);
+    const getProducts = async () => {
+        let url = 'http://localhost:4000/products'
+        let response = await fetch(url);
+        let data = await response.json();
+        // console.log(data);
+        setProductList(data);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
     return (
         <div>
+            <ProductCard/>
         </div>
     );
 };
