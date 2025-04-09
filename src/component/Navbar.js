d7b81b4e5f528deb6dffe90a218baaee79dbb242
jihyun1029 <parkji2028@naver.com>
2025-04-09 12:19:47 +0900

    Navbar 수정

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faUser} from '@fortawesome/free-regular-svg-icons'
 import {faBars, faSearch} from "@fortawesome/free-solid-svg-icons";
-import {useNavigate} from 'react-router-dom';
-import {Container} from "react-bootstrap";
+import {Link, useNavigate} from 'react-router-dom';
 
 const Navbar = () => {
     const menuList = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'H&M Home', 'Sale', '지속가능성'];
-
+    let [width, setWidth] = useState(0);
     const navigate = useNavigate()
     const goToLogin = () => {
         navigate('/login');
@@ -15,59 +14,48 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="pc">
-                <div className="login-wrap">
-                    <div className="login-button" onClick={goToLogin}>
-                        <FontAwesomeIcon icon={faUser} />
-                        <span>로그인</span>
-                    </div>
-                </div>
-                <div className="nav-section">
-                    <a href="/">
-                        <img
-                            width={100}
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/1418px-H%26M-Logo.svg.png"
-                        />
-                    </a>
-                </div>
-                <div className="menu-area">
-                    <ul className="menu-list">
-                        {menuList.map((menu, index) => (
-                            <li key={index}>{menu}</li>
-                        ))}
-                    </ul>
-                    <div className="search">
-                        <FontAwesomeIcon icon={faSearch} />
-                        <input type="text" placeholder="제품검색"/>
-                    </div>
+            <div className="side-menu" style={{ width: width }}>
+                <button className="closebtn" onClick={() => setWidth(0)}>
+                    &times;
+                </button>
+                <div className="side-menu-list" id="menu-list">
+                    {menuList.map((menu, index) => (
+                        <button key={index}>{menu}</button>
+                    ))}
                 </div>
             </div>
+            <div className="nav-header">
+                <div className="burger-menu hide">
+                    <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+                </div>
+                <div className="login-button" onClick={goToLogin}>
+                    <FontAwesomeIcon icon={faUser} />
+                    <div>로그인</div>
+                </div>
+            </div>
+            <div className="nav-logo">
+                <Link to="/">
+                    <img
+                        width={100}
+                        src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-1999-present.jpg"
+                    />
+                </Link>
+            </div>
+            <div className="nav-menu-area">
+                <ul className="menu">
+                    {menuList.map((menu, index) => (
+                        <li>
+                            <a href="#" key={index}>
+                                {menu}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
 
-            <div className="mobile">
-                <div className="nav-header">
-                    <div className="burger-menu">
-                        <FontAwesomeIcon icon={faBars} />
-                    </div>
-                    <div className="login-button" onClick={goToLogin}>
-                        <FontAwesomeIcon icon={faUser} />
-                        <span>로그인</span>
-                    </div>
+                <div className="search-box">
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input type="text" />
                 </div>
-                <div className="nav-section">
-                    <a href="/">
-                        <img
-                            width={100}
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/1418px-H%26M-Logo.svg.png"
-                        />
-                    </a>
-                </div>
-                {/*<div className="menu-area">*/}
-                {/*    <ul className="menu-list">*/}
-                {/*        {menuList.map((menu, index) => (*/}
-                {/*            <li key={index}>{menu}</li>*/}
-                {/*        ))}*/}
-                {/*    </ul>*/}
-                {/*</div>*/}
             </div>
         </div>
     );
